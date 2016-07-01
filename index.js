3006845dba945689b901458120da363b31efd016
khejing <khejing3@aliyun.com>
2016-07-01 20:08:32 +0800

    把没用的全局变量删掉

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 import Moment from 'moment';
-let localNotification;
-if(DEVICE_TYPE === 'mobile' && PLATFORM !== 'webapp' && NETWORK_TYPE === 'cordova'){
-    localNotification = cordova.require('de.appplant.cordova.plugin.local-notification.LocalNotification');
-}
+import {isMobile} from 'ua.js';
 
 function makeSystemNotification(title, msgText, customData){
-    if(DEVICE_TYPE === 'mobile' && PLATFORM !== 'webapp'){
-        if(NETWORK_TYPE === 'cordova'){
-            localNotification.schedule({
-                id: Moment().valueOf(),
-                title: title,
-                text: msgText,
-                data: customData
-            });
-        }else if(NETWORK_TYPE === 'websocket'){
-            simpleCordova.showNotification(title, msgText, JSON.stringify(JSON.stringify(customData)));
-        }
-    }
+    //TODO: using HTML5 notification API
 }
 
 function systemNotificationVibrate(){
-    if(DEVICE_TYPE === 'mobile' && PLATFORM !== 'webapp'){
+    if(isMobile()){
         //android 4.4.2 is chrome 30, don't have this method
         if(navigator.vibrate){
             navigator.vibrate(500);
@@ -34,7 +20,7 @@ function makeSystemNotificationWithVibration(title, msgText, customData){
 }
 
 function startCallNotification(){
-    if(DEVICE_TYPE === 'mobile' && PLATFORM !== 'webapp'){
+    if(isMobile()){
         //android 4.4.2 is chrome 30, don't have this method
         if(navigator.vibrate){
             navigator.vibrate([500, 500]);
@@ -43,7 +29,7 @@ function startCallNotification(){
 }
 
 function stopCallNotification(){
-    if(DEVICE_TYPE === 'mobile' && PLATFORM !== 'webapp'){
+    if(isMobile()){
         //android 4.4.2 is chrome 30, don't have this method
         if(navigator.vibrate){
             navigator.vibrate(0);
@@ -51,4 +37,4 @@ function stopCallNotification(){
     }
 }
 
-export {makeSystemNotification, systemNotificationVibrate, makeSystemNotificationWithVibration, localNotification as systemNotification};
+export {makeSystemNotification, systemNotificationVibrate, makeSystemNotificationWithVibration};
